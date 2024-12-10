10628f4c4f14dfeecc21d6a34c9b8cd26f2f0e3d
Maneesh-Kosuru <ksaimaneesh123@gmail.com>
2024-12-10 15:31:26 +0530

    12-10-2024:3.31PM

--- LLM_1/main.py
+++ LLM_1/main.py
@@ -1,4 +1,7 @@
 import os
-if __name__ == '__main__':
-    print('Hello langchain')
-    print(os.environ['OPEN_AI_API_KEY'])
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Hello langchain")
+    print(os.environ["OPEN_AI_API_KEY"])
